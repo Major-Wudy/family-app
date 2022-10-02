a40a218fca63256f7b449e6144c4ca75fc2707a9
Raphael Wudy <raphael.wudy94@web.de>
2022-10-02 12:40:00 +0200

    update registerform

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -11,46 +11,42 @@
             <div class="form-group text-center">
                 <x-icon type="register" :size="64"/>
             </div>
-            <div class="col">
-                <div class="row g-4">
-                    <div class="col-lg-2">
-                        <div class="form-floating">
-                            <input id="username" name="username" class="form-control" type="text" value="{{ old('username') }}" placeholder="@lang('register.placeholder-user')">
-                            <label for="username" >@lang('register.placeholder-user'):</label>
-                        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+                <div class="form-floating">
+                    <input id="forename" name="forename" class="form-control" type="text" value="{{ old('forename') }}" placeholder="@lang('register.placeholder-forename')">
+                    <label for="forename" >@lang('register.placeholder-forename'):</label>
+                </div>
+                <div class="form-floating">
+                    <input id="surname" name="surname" class="form-control" type="text" value="{{ old('surname') }}" placeholder="@lang('register.placeholder-surname')">
+                    <label for="surname" >@lang('register.placeholder-surname'):</label>
+                </div>
+
+                    <div class="form-floating">
+                        <input id="password" name="password" class="form-control" type="password" value="" placeholder="@lang('register.placeholder-password')">
+                        <label for="password">@lang('register.placeholder-password'):</label>
                     </div>
-                    <div class="col-lg-3">
-                        <div class="col">
-                            <div class="form-floating">
-                                <input id="password" name="password" class="form-control" type="password" value="" placeholder="@lang('register.placeholder-password')">
-                                <label for="password">@lang('register.placeholder-password'):</label>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="d-flex justify-content-end">
-                                <div class="input-group mb-4 justify-content-end">
-                                    <a id="show_pw" class="btn btn-link text-dark" onclick="pw_visible()">@lang('register.show-pw')</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- //TODO Convert into select with options read from DB --}}
-                    <div class="col-lg-3">
-                        <div class="form-floating">
-                            <input id="role" name="role" class="form-control" type="text" value="" placeholder="@lang('register.placeholder-role')">
-                            <label for="role">@lang('register.placeholder-role'):</label>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="form-floating">
-                            <input id="email" name="email" class="form-control" type="email" value="{{ old('email') }}" placeholder="@lang('register.placeholder-email')">
-                            <label for="email">@lang('register.placeholder-email'):</label>
+
+                <div class="form-group">
+                    <div class="d-flex justify-content-end">
+                        <div class="input-group mb-4 justify-content-end">
+                            <a id="show_pw" class="btn btn-link text-dark" onclick="pw_visible()">@lang('register.show-pw')</a>
                         </div>
                     </div>
                 </div>
+                <div class="form-floating">
+                    <input id="email" name="email" class="form-control" type="email" value="{{ old('email') }}" placeholder="@lang('register.placeholder-email')">
+                    <label for="email">@lang('register.placeholder-email'):</label>
+                </div>
 
+                {{-- //TODO Convert into select with options read from DB --}}
 
+                <div class="form-floating">
+                    <input id="role" name="role" class="form-control" type="text" value="" placeholder="@lang('register.placeholder-role')">
+                    <label for="role">@lang('register.placeholder-role'):</label>
+                </div>
             </div>
+        </div>
             <div class="input-group mt-4 d-grid gap-4">
                 <button class="btn btn-success" type="submit"><x-icon type="send"/>@lang('register.submit')</button>
             </div>
